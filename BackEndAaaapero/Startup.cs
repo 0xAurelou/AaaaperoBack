441e75e639218c0dbda2f219600697541a59d3db
Yanis Hermassi <yanis.hermassi@gmail.com>
2021-03-25 20:03:40 +0000

    fix

--- BackEndAaaapero/Startup.cs
+++ BackEndAaaapero/Startup.cs
@@ -10,6 +10,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BackEndAaaapero.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace BackEndAaaapero
 {
@@ -24,7 +26,12 @@ namespace BackEndAaaapero
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+        { 
+            services.AddDbContext<Context>(opt =>
+            {
+                opt.UseMySql(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            
             services.AddControllers();
         }
 
