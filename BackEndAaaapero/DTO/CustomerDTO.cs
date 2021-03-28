91dd0ef95a8d531fdbe11e918751e75bf0f0e591
Kiuchi <andy.kiuchi@hotmail.com>
2021-03-28 22:59:26 +0100

    AK DTO authentification with first_name and Last_Name

--- BackEndAaaapero/DTO/CustomerDTO.cs
+++ BackEndAaaapero/DTO/CustomerDTO.cs
@@ -1,12 +1,19 @@
 using System.Collections.Generic;
 using Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace DTO
 {
     public class CustomerDTO
     {
-        public int customer_id { get; set; }
+        public int Customer_id { get; set; }
+
         public string First_name {get; set;}
         public string Last_name { get; set; }
+        
+        [Required]
+        public string username { get; set; }
+        [Required]
+        public string password { get; set; }
     }
 }
